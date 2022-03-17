3bf3a05896a6233a6c8d6191a6cdf6773f9fdb95
zackmon <salemthunder.fist@gmail.com>
2022-03-17 22:39:27 +0300

    added psyq 4.4&4.7

---
+++ include/psyq4.7/pad.h
@@ -0,0 +1,37 @@
+#ifndef pad_h
+#define pad_h
+
+/* Controller Pad 1 Defines */
+#define Pad1Up          (1<<12)
+#define Pad1Down        (1<<14)
+#define Pad1Left        (1<<15)
+#define Pad1Right       (1<<13)
+#define Pad1L1          (1<< 2)
+#define Pad1L2          (1<< 0)
+#define Pad1R1          (1<< 3)
+#define Pad1R2          (1<< 1)
+#define Pad1tri         (1<< 4)
+#define Pad1sqr         (1<< 7)
+#define Pad1crc         (1<< 5)
+#define Pad1x           (1<< 6)
+#define Pad1Start       (1<<11)
+#define Pad1Select      (1<<8)
+
+/* Controller Pad 2 Defines */
+#define Pad2Up          (1<<28)
+#define Pad2Down        (1<<30)
+#define Pad2Left        (1<<31)
+#define Pad2Right       (1<<29)
+#define Pad2L1          (1<<18)
+#define Pad2L2          (1<<16)
+#define Pad2R1          (1<<19)
+#define Pad2R2          (1<<17)
+#define Pad2tri         (1<<20)
+#define Pad2sqr         (1<<23)
+#define Pad2crc         (1<<21)
+#define Pad2x           (1<<22)
+#define Pad2Start       (1<<27)
+#define Pad2Select      (1<<24)
+
+#endif
+
