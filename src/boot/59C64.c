90f994e5f6ec03ab105e35549e0b6090328b591f
Salem AL-Ghanim <salemthunder.fist@gmail.com>
2022-03-20 14:37:14 +0300

    matched CallCdSync_80069484

--- src/boot/59C64.c
+++ src/boot/59C64.c
@@ -1,5 +1,22 @@
 #include "common.h"
 
+extern int D_80088558;
+extern int CD_sync( int mode, unsigned char *result);
+
 INCLUDE_ASM("config/../asm/boot/nonmatchings/59C64", func_80069464);
 
-INCLUDE_ASM("config/../asm/boot/nonmatchings/59C64", func_80069484);
+/* int func_80069464(int arg0) {
+    
+    int var1;
+    var1 = D_80088558;
+    D_80088558 = arg0;
+    return var1;
+}  */
+
+//INCLUDE_ASM("config/../asm/boot/nonmatchings/59C64", CallCdSync_80069484);
+
+
+void CallCdSync_80069484(int arg0, unsigned char* arg1) {
+
+    CD_sync(arg0, arg1);
+}
