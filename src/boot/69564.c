d4a4d7487b5200e6b289be7894f52f16174b8c45
Salem AL-Ghanim <salemthunder.fist@gmail.com>
2022-03-20 14:29:29 +0300

    matched InitMemoryCard_80078D64

--- src/boot/69564.c
+++ src/boot/69564.c
@@ -1,3 +1,10 @@
 #include "common.h"
 
-INCLUDE_ASM("config/../asm/boot/nonmatchings/69564", InitMemoryCard_80078D64);
+//INCLUDE_ASM("config/../asm/boot/nonmatchings/69564", InitMemoryCard_80078D64);
+
+void InitMemoryCard_80078D64(int arg0) {
+    InitCARD_0(arg0);
+    StartCARD();
+    _bu_init();
+}
+
